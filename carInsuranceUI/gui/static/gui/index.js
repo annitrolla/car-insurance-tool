b66726671de77a260046705e3d930166e3606a28
Anna Leontjeva <annitrolla@gmail.com>
2017-07-30 19:50:43 +0300

    button style changes

--- carInsuranceUI/gui/static/gui/index.js
+++ carInsuranceUI/gui/static/gui/index.js
@@ -37,3 +37,24 @@ function getCookie(name) {
     return cookieValue;
 }
 
+// "Choose a file" button will change its name to name of the file
+var inputs = document.querySelectorAll( '#file-input' );
+Array.prototype.forEach.call( inputs, function( input )
+{
+    var label    = input.nextElementSibling,
+        labelVal = label.innerHTML;
+
+    input.addEventListener( 'change', function( e )
+    {
+        var fileName = '';
+        if( this.files && this.files.length > 1 )
+            fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+        else
+            fileName = e.target.value.split( '\\' ).pop();
+
+        if( fileName )
+            label.querySelector( 'span' ).innerHTML = fileName;
+        else
+            label.innerHTML = labelVal;
+    });
+});
